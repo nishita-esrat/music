d76a69918818e33f39035a0a0762096d24e89454
nishita-esrat <nishitaesrat120@gmail.com>
2024-06-29 00:15:39 +0600

    delete selected classes route is done

--- server/index.js
+++ server/index.js
@@ -660,6 +660,33 @@ async function run() {
         console.log(error);
       }
     });
+
+    // delete selected classes
+    app.put(
+      "/delete-select-class/:classId",
+      authenticated,
+      async (req, res) => {
+        try {
+          // get student
+          const student = await User.find({
+            email: req.authenticated_user.email,
+          }).toArray();
+          // check student or not
+          if (!(student[0].role == "student")) {
+            return res.status(403).json({ message: "Forbidden" });
+          }
+          // delete selected class
+          await User.updateOne(
+            { _id: student[0]._id },
+            { $pull: { select_class: req.params.classId } }
+          );
+          return res.status(200).json({ message: "class is deselected" });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
+    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
